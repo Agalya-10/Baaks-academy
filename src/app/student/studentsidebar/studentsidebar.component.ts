e513a08a512f0a1e16b4308771d6162cbed5019d
Agalya_10 <ayyappan.rajendran@icodexsolutions.local>
2026-01-19 09:31:55 +0530

    process

--- src/app/student/studentsidebar/studentsidebar.component.ts
+++ src/app/student/studentsidebar/studentsidebar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-studentsidebar',
   templateUrl: './studentsidebar.component.html',
   styleUrls: ['./studentsidebar.component.css']
 })
 export class StudentsidebarComponent {
+  constructor(private router:Router){}
+  logout()
+  {
+    console.log("Logout successfully")
+    this.router.navigate(['/signin'])
+  }
 
 }
