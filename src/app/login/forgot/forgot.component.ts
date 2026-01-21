892d1a78c3f3d390eaed3e5e1805824eb32b401c
Agalya_10 <ayyappan.rajendran@icodexsolutions.local>
2026-01-21 10:40:56 +0530

    otp route

--- src/app/login/forgot/forgot.component.ts
+++ src/app/login/forgot/forgot.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent {
+  constructor(private router :Router){}
+
+  otp_checker()
+  {
+    this.router.navigate(['otpgenerator'])
+  }
+
 
 }
